26725de15f8c7cfeee207849ad5b5ed073fceff4
s1ches <1chessmic@gmail.com>
2026-02-17 01:27:14 +0300

    add how to run, add dummy files check

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
+import os
 from Parser import Utils
 from Parser.WikiCrawler import WikiCrawler
 
+def crawl_required(output_dir="output", index_file="index.txt", min_files=100):
+    if not os.path.exists(output_dir):
+        return True
+
+    if not os.path.exists(index_file):
+        return True
+
+    txt_files = [
+        f for f in os.listdir(output_dir)
+        if f.endswith(".txt")
+    ]
+
+    if len(txt_files) < min_files:
+        return True
+
+    return False
 
 if __name__ == '__main__':
     crawler = WikiCrawler(
@@ -10,4 +27,8 @@ if __name__ == '__main__':
         index_file = Utils.INDEX_FILE
     )
 
-    crawler.crawl()
+    if crawl_required(Utils.OUTPUT_DIR, Utils.INDEX_FILE, Utils.MAX_PAGES):
+        print("Запускаем краулер...")
+        crawler.crawl()
+    else:
+        print("Файлы уже существуют. Краулер не запущен.")
